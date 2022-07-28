af18c1ac979c9cd8ce6b3f54fc1ee57e087331bb
afdavletkulov <afdavletkulov@rttv.ru>
2022-07-28 08:50:03 +0500

    dashboard

--- src/view/UI/pages/dashboard/components/Body.tsx
+++ src/view/UI/pages/dashboard/components/Body.tsx
@@ -1,17 +1,18 @@
 import Box from "@mui/material/Box";
 
 import { DrawerHeader } from "../../../common/DrawerHeader";
+import { IBody } from "../types";
 import { Chart } from "./Chart";
 
 import "./styles/Body.scss";
 
-export function Body() {
+export function Body({charts}: IBody) {
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
       <DrawerHeader />
       <div className="Body Body__chart-box">
         <h1>СВОДКА</h1>
-        <Chart />
+        <Chart charts={charts}/>
       </div>
     </Box>
   );
--- src/view/UI/pages/dashboard/index.tsx
+++ src/view/UI/pages/dashboard/index.tsx
@@ -1,12 +1,26 @@
-import Box from '@mui/material/Box';
-import { Body } from './components/Body';
-import { Sidebar } from './components/SideBar';
+import Box from "@mui/material/Box";
+import { Body } from "./components/Body";
+import { Sidebar } from "./components/SideBar";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_DASHBOARD_STAT } from "../../../../services/DashboardService";
+import { useEffect, useState } from "react";
+import { dashboardAdapter } from "../../../../utils/dashboardAdapter";
 
 export function Dashboard() {
+  const { loading, error, data } = useQuery(GET_ALL_DASHBOARD_STAT);
+  const [charts, setCharts] = useState(null)
+  
+  useEffect(() => {
+    if(data) {
+      setCharts(dashboardAdapter(data))
+    }
+  }, [data])
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar/>
-      <Body/>
+    <Box sx={{ display: "flex" }}>
+      <Sidebar />
+
+      <Body charts={charts}/>
     </Box>
   );
 }
