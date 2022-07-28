0c4e35fc3917ee875f936e7d64eba1dfac78d9da
afdavletkulov <afdavletkulov@rttv.ru>
2022-07-28 16:46:54 +0500

    firebase

--- src/view/UI/pages/login/components/FormButton.tsx
+++ src/view/UI/pages/login/components/FormButton.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress } from "@mui/material";
+import { Button } from "@mui/material";
 import { IFormButton } from "../types";
 
 function FormButton({ loading }: IFormButton) {
